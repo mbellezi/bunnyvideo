440f985885ebcc9f80f689fc1bd40875764b2b77
Marcos Bellezi <mbellezi@gmail.com>
2025-04-05 20:52:30 -0300

    up

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_bunnyvideo';  // Full name of the plugin (used for diagnostics)
-$plugin->version   = 2025040504;        // Versão incrementada novamente
+$plugin->version   = 2025040600;        // Versão incrementada novamente
 $plugin->requires  = 2021051700;        // Moodle 3.11+ (Adjust as needed for JS module/API usage)
 $plugin->maturity  = MATURITY_ALPHA;    // MATURITY_ALPHA, BETA, RC, or STABLE
 $plugin->release   = '0.1.0';
